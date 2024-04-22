0aec40cf25b46ba1203865eca83106e269cd0098
fakef0rest <fakef0rest69@gmail.com>
2024-04-22 14:41:26 +0200

    fix: error component

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,5 +1,6 @@
 import Paper from '@mui/material/Paper';
 import styles from './Error.module.scss';
+import { Alert, AlertTitle, Button } from '@mui/material';
 
 export const Error = () => {
   return (
@@ -7,7 +8,15 @@ export const Error = () => {
         <div className="row">
             <div className="col__12">
                 <div className={styles.error}>
-                    <Paper elevation={3}>Error. You are on the wrong page</Paper>
+                    <Paper className={styles.error__paper} elevation={3}>      
+                      <Alert className={styles.error__alert} severity="error">
+                        <AlertTitle>Error</AlertTitle>
+                        Page not Found 404
+                      </Alert>
+                      <Button href="/" variant="contained" color="error">
+                        Return to Home Page
+                      </Button>
+                    </Paper>
                 </div>
             </div>
         </div>
